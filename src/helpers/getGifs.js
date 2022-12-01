13ae366a27f96e7ea4a82e22a659e207f463d37d
Monica Rivera Pun <mondev811@gmail.com>
2022-11-30 22:34:58 -0800

    Remove deleted key

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,6 @@
 export const getGifs = async (category, count) => {
-  const url = `https://api.giphy.com/v1/gifs/search?api_key=VhWcUDpjN6eLQhAnPTNSAeKIl1loc3JC&q=${category}&limit=${count}`;
+  const api_key = "DTyOlNbhkABmTD0obzGKwiHoYKJTzjBb";
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${category}&limit=${count}`;
   const resp = await fetch(url);
   const { data } = await resp.json();
   const gifs = data.map((img) => ({
